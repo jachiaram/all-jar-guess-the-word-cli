7b6a0a4fe61befc4d02c56bf55a61eccf921ebf5
Eldubious <luke.livesaywright@aa.com>
2026-06-26 08:57:43 -0700

    added database url

--- src/app/database.py
+++ src/app/database.py
@@ -1,12 +1,11 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session, declarative_base
 from typing import Generator
 
-SQLALCHEMY_DATABASE_URL = os.environ["DATABASE_URL"]
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg://localhost/swurdal"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoFlush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
